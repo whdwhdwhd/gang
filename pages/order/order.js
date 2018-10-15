388abcb6a8d7d8b3977f10407c58f436e9aaae07
LJM <907124446@qq.com>
2018-10-16 07:49:56 +0800

    createdProject

---
+++ pages/order/order.js
@@ -0,0 +1,110 @@
+// pages/order/order.js
+var tabs = [
+  {
+    name: "全部"
+  },
+  {
+    name: "待服务"
+  },
+  {
+    name: "待付款"
+  },
+  {
+    name: "待评价"
+  }
+];
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    tabs: tabs,     //展示的数据
+    slideOffset: 0,//指示器每次移动的距离
+    activeIndex: 0,//当前展示的Tab项索引
+    sliderWidth: 96,//指示器的宽度,计算得到
+    contentHeight: 0//页面除去头部Tabbar后，内容区的总高度，计算得到
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+    var that = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        that.setData({
+          //计算相关宽度
+          sliderWidth: res.windowWidth / that.data.tabs.length,
+          sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex,
+          contentHeight: res.windowHeight - res.windowWidth / 750 * 68//计算内容区高度，rpx -> px计算
+        });
+      }
+    });
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  },
+  bindChange: function (e) {
+    var current = e.detail.current;
+    this.setData({
+      activeIndex: current,
+      sliderOffset: this.data.sliderWidth * current
+    });
+    console.log("bindChange:" + current);
+  },
+
+  navTabClick: function (e) {
+    this.setData({
+      sliderOffset: e.currentTarget.offsetLeft,
+      activeIndex: e.currentTarget.id
+    });
+    console.log("navTabClick:" + e.currentTarget.id);
+  }
+})
